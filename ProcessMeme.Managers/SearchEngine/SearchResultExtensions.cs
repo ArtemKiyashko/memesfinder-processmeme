d9fbc38642c591414898e944daf30769c5c1758a
Artem Kiyashko <artem86@hotmail.co.uk>
2022-12-20 20:40:30 +0100

    correct random result array bounds

--- ProcessMeme.Managers/SearchEngine/SearchResultExtensions.cs
+++ ProcessMeme.Managers/SearchEngine/SearchResultExtensions.cs
@@ -6,7 +6,7 @@ namespace ProcessMeme.Managers.SearchEngine
 	public static class SearchResultExtensions
 	{
 		public static Result? GetRandomResult(this IList<Result> results)
-			=> results is null ? default : results[Random.Shared.Next(0, results.Count - 1)];
+			=> results is null ? default : results[Random.Shared.Next(0, results.Count)];
 	}
 }
 
