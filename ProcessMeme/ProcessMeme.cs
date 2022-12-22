12f57f0122091f13e70080b8e759d9d6e6e62f40
KirillRukasov <shadowsun67@gmail.com>
2022-12-22 20:20:43 +0100

    Configuration for TelegramBotClient, add logic in ProcessMeme

--- ProcessMeme/ProcessMeme.cs
+++ ProcessMeme/ProcessMeme.cs
@@ -1,7 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using MemesFinderTextProcessor.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using ProcessMeme.Interfaces.SearchEngine;
+using System;
+using System.Threading.Tasks;
+using Telegram.Bot;
 
 namespace ProcessMeme
 {
@@ -9,17 +12,41 @@ namespace ProcessMeme
     {
         private readonly ILogger<ProcessMeme> _logger;
         private readonly IGoogleSearchEngineManager _googleSearchEngineManager;
+        private readonly ITelegramBotClient _telegramBotClient;
 
-        public ProcessMeme(ILogger<ProcessMeme> log, IGoogleSearchEngineManager googleSearchEngineManager)
+        public ProcessMeme(ILogger<ProcessMeme> log, IGoogleSearchEngineManager googleSearchEngineManager, ITelegramBotClient telegramBotClient)
         {
             _logger = log;
             _googleSearchEngineManager = googleSearchEngineManager;
+            _telegramBotClient = telegramBotClient;
         }
 
         [FunctionName("ProcessMeme")]
-        public async Task Run([ServiceBusTrigger("keywordmessages", "memeprocessor", Connection = "ServiceBusOptions")]string mySbMsg)
+        public async Task Run([ServiceBusTrigger("keywordmessages", "memeprocessor", Connection = "ServiceBusOptions")] TgMessageModel tgMessageModel)
         {
-            _logger.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
+            //keyword search
+            var memeLink = await _googleSearchEngineManager.GetMemeLinkAsync(tgMessageModel.Keyword);
+            //check if meme link is not null or empty
+            if (string.IsNullOrEmpty(memeLink))
+            {
+                _logger.LogError("Link is null or empty");
+                return;
+            }
+
+            //try send meme
+            try
+            {
+                await _telegramBotClient.SendPhotoAsync(
+                    chatId: tgMessageModel.Message.Chat.Id,
+                    replyToMessageId: tgMessageModel.Message.MessageId,
+                    photo: memeLink);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Cant send meme with URL: {memeLink}");
+                return;
+            }
+            Console.WriteLine(memeLink);
         }
     }
 }
--- ProcessMeme/Startup.cs
+++ ProcessMeme/Startup.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using Azure.Identity;
-using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using ProcessMeme.Extensions;
+using Microsoft.Extensions.Options;
 using ProcessMeme.Infrastructure.SearchEngine;
+using ProcessMeme.Options;
+using Telegram.Bot;
 
 [assembly: FunctionsStartup(typeof(ProcessMeme.Startup))]
 namespace ProcessMeme
 {
-	public class Startup : FunctionsStartup
-	{
+    public class Startup : FunctionsStartup
+    {
         private IConfigurationRoot _functionConfig;
 
         public override void Configure(IFunctionsHostBuilder builder)
@@ -20,8 +20,10 @@ namespace ProcessMeme
                 .Build();
 
             builder.Services.AddGoogleSearch(_functionConfig);
-
             builder.Services.AddLogging();
+
+            builder.Services.Configure<TelegramBotOptions>(_functionConfig.GetSection("TelegramBotOptions"));
+            builder.Services.AddSingleton<ITelegramBotClient>(factory => new TelegramBotClient(factory.GetService<IOptions<TelegramBotOptions>>().Value.Token));
         }
     }
 }
