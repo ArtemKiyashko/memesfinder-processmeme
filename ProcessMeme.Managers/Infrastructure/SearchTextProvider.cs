1f99642be3f0951dc14591961e1e1b711413f34f
KirillRukasov <shadowsun67@gmail.com>
2022-12-25 14:51:11 +0100

    search by twitter

--- ProcessMeme.Managers/Infrastructure/SearchTextProvider.cs
+++ ProcessMeme.Managers/Infrastructure/SearchTextProvider.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using ProcessMeme.Interfaces.Infrastructure;
+﻿using ProcessMeme.Interfaces.Infrastructure;
 
 namespace ProcessMeme.Managers.Infrastructure
 {
-	public class SearchTextProvider : ISearchTextProvider
-	{
-        public string GetSearchText(string message) => $"{message} мем";
+    public class SearchTextProvider : ISearchTextProvider
+    {
+        public string GetSearchText(string message) => $"site:twitter.com {message} мем";
     }
 }
 
