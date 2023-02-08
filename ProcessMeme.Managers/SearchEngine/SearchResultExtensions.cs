fca3c849b72514136ae8be6a14891ca635e7eae4
Artem Kiyashko <artem86@hotmail.co.uk>
2023-02-08 12:51:01 +0100

    choose meme from first 3 results

--- ProcessMeme.Managers/SearchEngine/SearchResultExtensions.cs
+++ ProcessMeme.Managers/SearchEngine/SearchResultExtensions.cs
@@ -6,7 +6,7 @@ namespace ProcessMeme.Managers.SearchEngine
 	public static class SearchResultExtensions
 	{
 		public static Result? GetRandomResult(this IList<Result> results)
-			=> results is null ? default : results[Random.Shared.Next(0, results.Count)];
+			=> results is null ? default : results[Random.Shared.Next(0, Math.Min(3, results.Count))];
 	}
 }
 
